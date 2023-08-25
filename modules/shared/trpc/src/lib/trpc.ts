c05ee9c00b9fd9d2f4199017e498d90c7acb3ae3
Edun Omatseye <edunomatseye@yahoo.com>
2023-08-25 20:38:33 +0100

    chore: trpc initialization

--- modules/shared/trpc/src/lib/trpc.ts
+++ modules/shared/trpc/src/lib/trpc.ts
@@ -1,3 +1,15 @@
 export function modulesSharedTrpc(): string {
   return 'modules-shared-trpc';
 }
+
+import { initTRPC } from '@trpc/server';
+import superjson from 'superjson';
+
+import { createContext } from './context';
+
+export const t = initTRPC.context<typeof createContext>().create({
+  transformer: superjson,
+  errorFormatter({ shape }) {
+    return shape;
+  },
+});
